a2d071dbffc0b5f11af71077f9eba379cab239da
Sangamesh SY <sangameshs@asl.com>
2021-05-12 14:20:27 +0530

    Commit by at 12-05-2021 14:20

--- src/Test/com/TC_TB.java
+++ src/Test/com/TC_TB.java
@@ -134,6 +134,7 @@ public class TC_TB extends Generic
 //BJBBHljgkgkv
 
 // Below Test Case is added on 12-05-2021 at 12:51 PM by Mr. XXX
+//To check so and so functionality..
    @Test
   public void testqq()
   {
